dbea8df21735aa3150a372c548f9f90f504f4bf2
Meet Patel <imeettpatel@gmail.com>
2024-09-04 17:07:40 +0530

    Created first spring project and print data using xml file

---
+++ Spring/src/main/java/com/spring/Spring/Person.java
@@ -0,0 +1,48 @@
+package com.spring.Spring;
+
+public class Person {
+	private int pid;
+	private String mobile_no;
+	private String email;
+	private String name;
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Person(int pid, String mobile_no, String email, String name) {
+		super();
+		this.pid = pid;
+		this.mobile_no = mobile_no;
+		this.email = email;
+		this.name = name;
+	}
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getMobile_no() {
+		return mobile_no;
+	}
+	public void setMobile_no(String mobile_no) {
+		this.mobile_no = mobile_no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Person [pid=" + pid + ", mobile_no=" + mobile_no + ", email=" + email + ", name=" + name + "]";
+	}
+	
+}
